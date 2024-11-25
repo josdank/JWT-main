8f835d067c133a791a429a7c662b3f9aa4fd0299
Safronjos <josue.guerra@epn.edu.ec>
2024-11-25 13:41:55 -0500

    First commit 25-11-2024

--- APIRESTFUL-TOURS-JWT/src/server.js
+++ APIRESTFUL-TOURS-JWT/src/server.js
@@ -6,17 +6,18 @@ import fileUpload from 'express-fileupload'
 import dotenv from 'dotenv'
 
 import routerTour from './routers/tour_routes.js'
-
 import routerUser from './routers/user_routes.js'
 
+dotenv.config()
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
 
 // Inicializaciones
 const app = express()
 
 cloudinary.config({ 
-    cloud_name: 'demo_web', 
-    api_key: '123', 
-    api_secret: '456' // Click 'View API Keys' above to copy your API secret
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
 // Variables 
 app.set('port', process.env.puertito || 3000)
